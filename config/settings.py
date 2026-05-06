1462ca20869a6339dd097d5fb6a2e5a886a8a1d2
Seyed Hossein Mousavi <shmusavi1994@gmail.com>
2026-05-06 19:31:21 +0330

    add books app

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
     # local app
     'accounts',
+    'pages',
+    'books',
     'widget_tweaks',
 ]
 
@@ -121,7 +123,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
