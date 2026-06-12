bb41dea320a7b5e223b3325e03ac1ac0bbd0e471
Seyed Hossein Mousavi <shmusavi1994@gmail.com>
2026-06-12 14:23:58 +0330

    Change Style Auth

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,11 +5,28 @@ from accounts.models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        # fields = UserCreationForm.Meta.fields + ('age',)
-        fields = ('username','first_name','last_name', 'email', 'age')
+        fields = ('username', 'first_name', 'last_name', 'email', 'age')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'نام کاربری'
+        self.fields['first_name'].label = 'نام'
+        self.fields['last_name'].label = 'نام خانوادگی'
+        self.fields['email'].label = 'ایمیل'
+        self.fields['age'].label = 'سن'
+        self.fields['password1'].label = 'رمز عبور'
+        self.fields['password2'].label = 'تکرار رمز عبور'
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        # fields = UserChangeForm.Meta.fields
         fields = ('username', 'first_name', 'last_name', 'email', 'age')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'نام کاربری'
+        self.fields['first_name'].label = 'نام'
+        self.fields['last_name'].label = 'نام خانوادگی'
+        self.fields['email'].label = 'ایمیل'
+        self.fields['age'].label = 'سن'
